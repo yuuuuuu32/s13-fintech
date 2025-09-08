87528856a581cef9b72836cdde454156ff920e57
하승재 <sj_hahaha@naver.com>
2025-09-08 15:26:20 +0900

    fix : 마피아흔적지우기1

--- finble-backend/src/main/java/com/ssafy/BlueMarble/MainApplication.java
+++ finble-backend/src/main/java/com/ssafy/BlueMarble/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MafiaApplication {
+public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MafiaApplication.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 
 }
