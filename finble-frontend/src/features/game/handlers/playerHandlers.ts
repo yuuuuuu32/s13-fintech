e78751b2d32329ffaa41d9ebb3bfb079df7b7fd0
박재연 <jaeyoun310@gmail.com>
2025-09-19 09:22:45 +0900

    fix: 감옥, 게임 로딩 시 생기던 문제

--- finble-frontend/src/features/game/handlers/playerHandlers.ts
+++ finble-frontend/src/features/game/handlers/playerHandlers.ts
@@ -234,7 +234,10 @@ export const createPlayerActions = (
           modal: {
             type: "INFO" as const,
             text: `감옥 탈출까지 ${newJailTurns}턴 남았습니다.`,
-            onConfirm: () => set({ modal: { type: "NONE" as const } }),
+            onConfirm: () => {
+              set({ modal: { type: "NONE" as const } });
+              get().endTurn();
+            },
           },
         };
       }
