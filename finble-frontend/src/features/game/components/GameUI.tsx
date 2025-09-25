6248646da2f333512649836e1a3fa63ad532cd7e
yuuuuuu32 <guuuuu32mi@gmail.com>
2025-09-25 21:40:19 +0900

    모달 디자인

--- finble-frontend/src/features/game/components/GameUI.tsx
+++ finble-frontend/src/features/game/components/GameUI.tsx
@@ -660,7 +660,7 @@ const ManagePropertyModalContent = ({
   );
 };
 
-// BuySpecialLandModalContent
+// BuySpecialLandModalContent!
 const BuySpecialLandModalContent = ({
   modal,
   buySpecialLand,
